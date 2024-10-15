8cdfb2527bb1b172b34d455f9f71833e84f95147
Hafsa. <178737731+HM-04@users.noreply.github.com>
2024-10-15 16:25:56 +0100

    Modify to include celsius and fahrenheit conversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,20 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f_temperature) {
+  if (isNaN(f_temperature)) {
+    return "ERROR"
+  }
+  let celsius = ((f_temperature - 32) * 5) / 9
+  celsius = celsius.toFixed(1)
+  return celsius
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c_temperature) {
+  if (isNaN(c_temperature)) {
+    return "ERROR"
+  }
+  let fahrenheit = (c_temperature * 9/5) + 32
+  fahrenheit = fahrenheit.toFixed(1)
+  return fahrenheit
 };
 
 // Do not edit below this line
