e66dc4eec7a890d141c751adf654141292d85876
Hafsa. <178737731+HM-04@users.noreply.github.com>
2024-10-15 15:59:04 +0100

    Modify sumAll.ja

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function(number1, number2) {
+   if (number1 || number2 <= 0) {
+    return ERROR
+   } 
+   let number3 = ""
+   for (let i = number1; i <= number2; i++){
+   number3 += i
+   
+   };
+   return number3
 };
 
 // Do not edit below this line
