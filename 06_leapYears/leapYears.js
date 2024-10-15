7bce6e4bc74ad8f2861a30fbb313b58fd9e0e2a5
Hafsa. <178737731+HM-04@users.noreply.github.com>
2024-10-15 16:10:35 +0100

    Modify leapYears.js to include function

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,12 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    if (isNaN(year)) {
+        return "ERROR"
+    };
+    if (year % 4 === 0 && (year % 400 === 0 || year % 100 != 0)) {
+        return "This is a leap year"
+    } else {
+        return "This is not a leap year"
+    }
 
 };
 
